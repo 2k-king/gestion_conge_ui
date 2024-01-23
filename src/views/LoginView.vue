f8ecf1f8d5c495d7be672a25ccd7174d1a58fc5f
2k-king <2kteambusiness@gmail.com>
2024-01-23 18:49:42 +0200

    init projrct

---
+++ src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <form>
+            <input type="text" placeholder="Username" v-model="username">
+            <input type="password" placeholder="password"v-model="password">
+            <button type="submit"v-on:click="performLogin">Login</button>
+        </form>
+        <p>Username={{ username }}</p>
+        <p>Password={{ password }}</p>
+        <div>votre identifiant:<strong></strong></div>
+
+    </div>
+</template>
+
+<script>
+
+export default{
+data(){
+return{
+    username:"",
+    password:"",
+}
+},
+methods:{
+    performLogin
+}
+}
+</script>
+
+<style scoped></style>
